ee4518e19b9c395149c88cef8c917b9bc3e115e9
Eskil Alstad <eskilal@stud.ntnu.no>
2022-12-01 19:26:36 +0100

    First commit
    First Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        System.out.println("Stop talking to me Arthur");
+        System.out.println("Nei arthur");
+    }
+}
